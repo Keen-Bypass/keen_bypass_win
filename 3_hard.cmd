775f2296b205178c48a2ff385b65a0304fc56a78
Nik <75453222+nikrays@users.noreply.github.com>
2025-04-23 14:40:55 +0300

    Update 3_hard.cmd

--- 3_hard.cmd
+++ 3_hard.cmd
@@ -3,10 +3,10 @@
 
 set ARGS=^
 --wf-tcp=80,443 --wf-udp=443,50000-50099 ^
---filter-tcp=80,443 --dpi-desync=fakeddisorder --dpi-desync-fooling=md5sig --wssize 1:6 --dpi-desync-split-pos=1 --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --hostlist=\"C:\keen_bypass_for_windows\keen-dpi-for-windows\files\list-googlevideo.txt\" --new ^
---filter-tcp=80,443 --dpi-desync=fakeddisorder --dpi-desync-fooling=md5sig --wssize 1:6 --dpi-desync-split-pos=1 --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --hostlist=\"C:\keen_bypass_for_windows\keen-dpi-for-windows\files\list-antifilter.txt\" --hostlist=\"C:\keen_bypass_for_windows\keen-dpi-for-windows\files\list-rkn.txt\" --hostlist-auto=\"C:\keen_bypass_for_windows\keen-dpi-for-windows\files\list-auto.txt\" --hostlist-exclude=\"C:\keen_bypass_for_windows\keen-dpi-for-windows\files\list-exclude.txt\" --new ^
---filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=20 --dpi-desync-fake-quic=\"C:\keen_bypass_for_windows\zapret-win-bundle-master\zapret-winws\files\quic_initial_www_google_com.bin\" --hostlist=\"C:\keen_bypass_for_windows\keen-dpi-for-windows\files\list-googlevideo.txt\" --new ^
---filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=20 --hostlist=\"C:\keen_bypass_for_windows\keen-dpi-for-windows\files\list-antifilter.txt\" --hostlist=\"C:\keen_bypass_for_windows\keen-dpi-for-windows\files\list-rkn.txt\" --hostlist-auto=\"C:\keen_bypass_for_windows\keen-dpi-for-windows\files\list-auto.txt\" --hostlist-exclude=\"C:\keen_bypass_for_windows\keen-dpi-for-windows\files\list-exclude.txt\" --new ^
+--filter-tcp=80,443 --dpi-desync=fakeddisorder --dpi-desync-fooling=md5sig --wssize 1:6 --dpi-desync-split-pos=1 --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --hostlist=\"C:\keen_bypass_win\keen_bypass_win\files\list-googlevideo.txt\" --new ^
+--filter-tcp=80,443 --dpi-desync=fakeddisorder --dpi-desync-fooling=md5sig --wssize 1:6 --dpi-desync-split-pos=1 --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --hostlist=\"C:\keen_bypass_win\keen_bypass_win\files\list-antifilter.txt\" --hostlist=\"C:\keen_bypass_win\keen_bypass_win\files\list-rkn.txt\" --hostlist-auto=\"C:\keen_bypass_win\keen_bypass_win\files\list-auto.txt\" --hostlist-exclude=\"C:\keen_bypass_win\keen_bypass_win\files\list-exclude.txt\" --new ^
+--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=20 --dpi-desync-fake-quic=\"C:\keen_bypass_win\zapret-win-bundle-master\zapret-winws\files\quic_initial_www_google_com.bin\" --hostlist=\"C:\keen_bypass_win\keen_bypass_win\files\list-googlevideo.txt\" --new ^
+--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=20 --hostlist=\"C:\keen_bypass_win\keen_bypass_win\files\list-antifilter.txt\" --hostlist=\"C:\keen_bypass_win\keen_bypass_win\files\list-rkn.txt\" --hostlist-auto=\"C:\keen_bypass_win\keen_bypass_win\files\list-auto.txt\" --hostlist-exclude=\"C:\keen_bypass_win\keen_bypass_win\files\list-exclude.txt\" --new ^
 --filter-udp=50000-50099 --filter-l7=discord,stun --dpi-desync=fake
 
 call :srvinst winws1
@@ -17,6 +17,6 @@ goto :eof
 :srvinst
 net stop %1
 sc delete %1
-sc create %1 binPath= "\"C:\keen_bypass_for_windows\zapret-win-bundle-master\zapret-winws\winws.exe\" %ARGS%" DisplayName= "zapret DPI bypass : %1" start= auto
+sc create %1 binPath= "\"C:\keen_bypass_win\zapret-win-bundle-master\zapret-winws\winws.exe\" %ARGS%" DisplayName= "zapret DPI bypass : %1" start= auto
 sc description %1 "zapret DPI bypass software"
 sc start %1
