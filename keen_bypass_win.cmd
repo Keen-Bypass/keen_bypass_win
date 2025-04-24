676b0bc2eeca4c768585cec6d3cf7c1fc3697b7c
Nik <75453222+nikrays@users.noreply.github.com>
2025-04-24 22:43:16 +0300

    Update keen_bypass_win.cmd

--- keen_bypass_win.cmd
+++ keen_bypass_win.cmd
@@ -1,25 +1,19 @@
 @echo off
 chcp 1251 >nul
 SETLOCAL ENABLEEXTENSIONS
-
 NET FILE >NUL 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     PowerShell -Command "Start-Process cmd -ArgumentList '/c chcp 1251>nul & %~dpnx0' -Verb RunAs"
     EXIT /B
 )
-
 SET "SCRIPT_URL=https://raw.githubusercontent.com/Keen-Bypass/keen_bypass_win/main/common/netinstall.cmd"
 SET "SAVE_PATH=%TEMP%\keen_bypass.cmd"
-
-PowerShell -Command "[IO.File]::WriteAllText('%SAVE_PATH%', (Invoke-WebRequest -Uri '%SCRIPT_URL%').Content, [Text.Encoding]::GetEncoding(1251))"
-
+PowerShell -Command "[IO.File]::WriteAllText('%SAVE_PATH%', (Invoke-WebRequest -Uri '%SCRIPT_URL%' -UseBasicParsing).Content, [Text.Encoding]::GetEncoding(1251))"
 IF NOT EXIST "%SAVE_PATH%" (
     echo Îøèáêà çàãðóçêè
     PAUSE
     EXIT /B 1
 )
-
 cmd /c chcp 1251>nul & call "%SAVE_PATH%"
-
 DEL /F /Q "%SAVE_PATH%" >NUL 2>&1
 EXIT /B
