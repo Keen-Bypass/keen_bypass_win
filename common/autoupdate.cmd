b56cff04eee5c6a88752dddbd6719aafb7b193e2
Nik <75453222+nikrays@users.noreply.github.com>
2025-06-02 18:56:15 +0300

    Update autoupdate.cmd

--- common/autoupdate.cmd
+++ common/autoupdate.cmd
@@ -41,6 +41,40 @@ if "!REMOTE_VERSION!" == "!LOCAL_VERSION!" (
 )
 
 :: ############################
+:: ## СОХРАНЕНИЕ ТЕКУЩЕЙ СТРАТЕГИИ ##
+:: ############################
+for /f "usebackq" %%i in (`powershell -Command "[Environment]::GetFolderPath('LocalApplicationData')"`) do set "LOCALAPPDATA_PATH=%%i"
+set "STRATEGY_FOLDER=%LOCALAPPDATA_PATH%\keen_bypass_win"
+set "STRATEGY=1"
+set "BASE_DIR=C:\keen_bypass_win\keen_bypass_win"
+
+:: Определение текущей стратегии
+if exist "%BASE_DIR%\1_easy.cmd" (
+    for /f %%F in ('sc query winws1 ^| findstr "RUNNING"') do set "STRATEGY=1"
+)
+if exist "%BASE_DIR%\2_medium.cmd" (
+    for /f %%F in ('sc query winws1 ^| findstr "RUNNING"') do (
+        if exist "%BASE_DIR%\2_medium.cmd" set "STRATEGY=2"
+    )
+)
+if exist "%BASE_DIR%\3_hard.cmd" (
+    for /f %%F in ('sc query winws1 ^| findstr "RUNNING"') do (
+        if exist "%BASE_DIR%\3_hard.cmd" set "STRATEGY=3"
+    )
+)
+if exist "%BASE_DIR%\4_extreme.cmd" (
+    for /f %%F in ('sc query winws1 ^| findstr "RUNNING"') do (
+        if exist "%BASE_DIR%\4_extreme.cmd" set "STRATEGY=4"
+    )
+)
+
+:: Сохранение стратегии
+mkdir "%STRATEGY_FOLDER%" >nul 2>&1
+del /Q /F "%STRATEGY_FOLDER%\*.txt" >nul 2>&1
+echo. > "%STRATEGY_FOLDER%\%STRATEGY%.txt"
+echo [ИНФО] Сохранена стратегия: %STRATEGY%
+
+:: ############################
 :: ## ЗАПУСК ОБНОВЛЕНИЯ ##
 :: ############################
 SET "SCRIPT_URL=https://raw.githubusercontent.com/Keen-Bypass/keen_bypass_win/main/common/netupdate.cmd"
