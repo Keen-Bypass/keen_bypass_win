cc99c857524f7f71d4bdb7905d162864657f696b
Nik <75453222+nikrays@users.noreply.github.com>
2025-09-16 15:12:37 +0300

    Update strategy2.cmd

--- presets/strategy2.cmd
+++ presets/strategy2.cmd
@@ -3,7 +3,7 @@
 
 set ARGS=^
 --wf-tcp=80,443 --wf-udp=443,1024-65535 ^
---filter-tcp=80,443 --dpi-desync=fake --dpi-desync-fooling=badsum,badseq,md5sig,ts --dpi-desync-fake-tls=0x00000000 --dpi-desync-fake-tls=! --dpi-desync-fake-tls-mod=rnd,rndsni,dupsid --dpi-desync-fake-tls-mod=rnd,dupsid,sni=rknpidor.google.com --hostlist-domains=googlevideo.com --new ^
+--filter-tcp=80,443 --dpi-desync=fake --dpi-desync-fooling=badseq,md5sig,ts --dpi-desync-fake-tls=0x00000000 --dpi-desync-fake-tls=! --dpi-desync-fake-tls-mod=rnd,rndsni,dupsid --dpi-desync-fake-tls-mod=rnd,dupsid,sni=rknpidor.google.com --hostlist-domains=googlevideo.com --new ^
 --filter-tcp=80,443 --dpi-desync=fake,multisplit --dpi-desync-fooling=badseq --dpi-desync-split-pos=1 --dpi-desync-fake-tls=0x00000000 --dpi-desync-fake-tls=! --dpi-desync-fake-tls-mod=rnd,rndsni,dupsid --hostlist=\"C:\ProgramData\keen_bypass_win\keen_bypass\files\hosts-antifilter.txt\" --hostlist=\"C:\ProgramData\keen_bypass_win\keen_bypass\files\hosts-rkn.txt\" --hostlist-auto=\"C:\ProgramData\keen_bypass_win\keen_bypass\files\hosts-auto.txt\" --hostlist-exclude=\"C:\ProgramData\keen_bypass_win\keen_bypass\files\hosts-exclude.txt\" --new ^
 --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=50 --dpi-desync-fake-quic=\"C:\ProgramData\keen_bypass_win\zapret-win-bundle-master\zapret-winws\files\quic_initial_www_google_com.bin\" --hostlist-domains=googlevideo.com --new ^
 --filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=12 --dpi-desync-fake-quic=0x4383a71211223344 --hostlist=\"C:\ProgramData\keen_bypass_win\keen_bypass\files\hosts-antifilter.txt\" --hostlist=\"C:\ProgramData\keen_bypass_win\keen_bypass\files\hosts-rkn.txt\" --hostlist-auto=\"C:\ProgramData\keen_bypass_win\keen_bypass\files\hosts-auto.txt\" --hostlist-exclude=\"C:\ProgramData\keen_bypass_win\keen_bypass\files\hosts-exclude.txt\" --new ^
@@ -21,3 +21,4 @@ sc create %1 binPath= "\"C:\ProgramData\keen_bypass_win\zapret-win-bundle-master
 sc description %1 "zapret DPI bypass software"
 
 sc start %1
+
