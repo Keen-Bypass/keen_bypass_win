d979239b0e3cd04f5b358138476f48745c4749d0
Nik <75453222+nikrays@users.noreply.github.com>
2025-09-21 16:20:36 +0300

    Update keen_bypass_win.cmd

--- keen_bypass_win.cmd
+++ keen_bypass_win.cmd
@@ -1,19 +1,4 @@
 @echo off
 chcp 1251 >nul
-SETLOCAL ENABLEEXTENSIONS
-NET FILE >NUL 2>&1
-IF %ERRORLEVEL% NEQ 0 (
-    PowerShell -Command "Start-Process cmd -ArgumentList '/c chcp 1251>nul & %~dpnx0' -Verb RunAs"
-    EXIT /B
-)
-SET "SCRIPT_URL=https://raw.githubusercontent.com/Keen-Bypass/keen_bypass_win/main/common/netinstall.cmd"
-SET "SAVE_PATH=%TEMP%\keen_bypass.cmd"
-PowerShell -Command "[IO.File]::WriteAllText('%SAVE_PATH%', (Invoke-WebRequest -Uri '%SCRIPT_URL%' -UseBasicParsing).Content, [Text.Encoding]::GetEncoding(1251))"
-IF NOT EXIST "%SAVE_PATH%" (
-    echo Îøèáêà çàãðóçêè
-    PAUSE
-    EXIT /B 1
-)
-cmd /c chcp 1251>nul & call "%SAVE_PATH%"
-DEL /F /Q "%SAVE_PATH%" >NUL 2>&1
-EXIT /B
+NET FILE >NUL 2>&1 || (PowerShell -Command "Start-Process cmd -ArgumentList '/c \"%~f0\"' -Verb RunAs" & EXIT /B)
+PowerShell -Command "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force; $u='https://raw.githubusercontent.com/Keen-Bypass/keen_bypass_win/main/common/netinstall.cmd'; $p='%TEMP%\k.cmd'; try {[IO.File]::WriteAllText($p, (iwr $u -UseBasicParsing).Content, [Text.Encoding]::GetEncoding(1251)); & $p; ri $p} catch {echo 'Error'; pause; exit 1}"
