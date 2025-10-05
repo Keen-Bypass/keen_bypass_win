b786d7f519beb5c6f16d64fcde8e940e84064a6c
Nik <75453222+nikrays@users.noreply.github.com>
2025-10-05 11:24:51 +0000

    update

--- common/netupdate.cmd
+++ common/netupdate.cmd
@@ -105,7 +105,7 @@ exit /b 0
     exit /b 1
 
 :GET_CURRENT_PRESET
-    set "PRESET=4"
+    set "PRESET=1"
 ::    if exist "%BACKUP_DIR%" (
 ::        for /f "delims=" %%f in ('dir /b "%BACKUP_DIR%\*.cmd" 2^>nul') do (
 ::            set "FILENAME=%%~nf"
